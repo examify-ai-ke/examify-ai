8aa9250a982ede01d7d193b198f40dc6fb676e51
migoya2020 <dmigoya@gmail.com>
2026-03-28 01:38:04 +0300

    updTED RECAPTURE

--- src/components/providers/captcha-provider.tsx
+++ src/components/providers/captcha-provider.tsx
@@ -6,8 +6,13 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 export function CaptchaProvider({ children }: { children: React.ReactNode }) {
   const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
 
+  React.useEffect(() => {
+    if (!siteKey) {
+      console.warn('reCAPTCHA site key is missing in CaptchaProvider. If you just added it to .env, please restart your dev server.');
+    }
+  }, [siteKey]);
+
   if (!siteKey) {
-    console.warn('reCAPTCHA site key is missing. Captcha will not be active.');
     return <>{children}</>;
   }
 
